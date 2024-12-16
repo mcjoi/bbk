edd0660f231d97de3480c965a0c5159726c69f72
bong <mcjoi@naver.com>
2024-12-16 16:21:09 +0900

    _1216

--- 02search.py
+++ 02search.py
@@ -4,8 +4,13 @@ import data as d
 df02 = d.df_mtcars.copy()
 
 # change index number from 1
-df02.index += 1
+# df02.index += 1
 
 # 1,2,3번 인덱스와 label index 번호가 다름에 주의!
-print(df02.iloc[[1,2,3],:])
-print(df02.loc[[1,2,3],:])
+# print(df02.iloc[[1,2,3],:])
+# print(df02.loc[[1,2,3],:])
+
+# 컬럼명 바꾸기
+df02.rename(columns={'cyl':'cyl2'}, inplace=True)
+print(df02.head(3))
+
