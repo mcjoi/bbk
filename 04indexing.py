a0bb13c4f04cfc11f68d93683bec9bb8f7a3e547
bong <mcjoi@naver.com>
2024-12-16 19:28:30 +0900

    _241216

---
+++ 04indexing.py
@@ -0,0 +1,18 @@
+import data as d
+df03 = d.df_iris.copy()
+
+# select column : 0, 4
+df04 = df03.iloc[:, 0:100]
+
+# rename specific column
+df04.rename(columns={'variety': 'Variety'}, inplace=True)
+
+print(df04.head(10))
+
+print(df04.loc[:,['Variety', 'sepal.length']].head(10))
+
+
+
+
+
+
